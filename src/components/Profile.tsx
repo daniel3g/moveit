dce55a26b03a2f138d82e45caa81c890621b59e4
Daniel da Silva <silvadaniel2006@yahoo.com.br>
2021-03-02 16:56:09 -0300

    profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/daniel3g.png" alt="Daniel"/>
+      <div>
+        <strong>Daniel Silva</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
