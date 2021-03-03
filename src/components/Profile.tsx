a93f3e84b4c8833ae94679fe2eb6abb3032fa3c3
Daniel da Silva <silvadaniel2006@yahoo.com.br>
2021-03-02 22:42:07 -0300

    criados os modulos profile e challenges

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
 export function Profile() {
   return (
-    <div>
+    <div className={styles.profileContainer}>
       <img src="https://github.com/daniel3g.png" alt="Daniel"/>
       <div>
         <strong>Daniel Silva</strong>
-        <p>Level 1</p>
+        
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level 1
+        </p>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { CompletedChallenges } from "../components/CompletedChallenges";
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -11,10 +12,11 @@ export default function Home() {
       <section>
       <div>
         <Profile />
+        <CompletedChallenges />
       </div>
-
+      
       <div>
-        
+      
       </div>
     </section>
     </div>
