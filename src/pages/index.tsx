e9df2ed580e840140f0c02761efe84d7b4403262
Daniel da Silva <silvadaniel2006@yahoo.com.br>
2021-03-03 21:23:55 -0300

    funcionalidade do countdown feita, faltando a funcionalidade de pausar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
+import { CountDown } from "../components/CountDown";
 
 export default function Home() {
   return (
-    <div className={ styles.container }>     
+    <div className={ styles.container }>
+      <Head>
+        <title>Move It</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
       <div>
         <Profile />
         <CompletedChallenges />
+        <CountDown />
       </div>
       
       <div>
