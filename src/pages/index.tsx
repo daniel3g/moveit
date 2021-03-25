cb400e3b21453a10635969538a21dd708504fc21
Daniel da Silva <silvadaniel2006@yahoo.com.br>
2021-03-25 12:49:06 -0300

    contextos criados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import Head from 'next/head';
+
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
-import Head from 'next/head';
+import { ChallengeBox } from "../components/ChallengeBox";
+import { CountDown } from "../components/CountDown";
 
 import styles from '../styles/pages/Home.module.css';
-import { CountDown } from "../components/CountDown";
 
 export default function Home() {
   return (
@@ -23,7 +25,7 @@ export default function Home() {
       </div>
       
       <div>
-      
+        <ChallengeBox />
       </div>
     </section>
     </div>
